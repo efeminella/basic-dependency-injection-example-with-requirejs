b49fdb9b0b57694fdfbe1949c93b7ef16b4c168c
Eric Feminella <efeminella@gmail.com>
2012-12-22 01:29:48 -0500

    Update src/shared/app/templates/Templates.js

--- src/shared/app/templates/Templates.js
+++ src/shared/app/templates/Templates.js
@@ -3,8 +3,8 @@ define( function( require ) {
     var templates = require('underscore').template
       , source    = require('TemplateSource');
     return {
-		someView: function() {
-			return templates(source.someViewTpl)();
-		}
+	someView: function() {
+	    return templates(source.someViewTpl)();
+	}
     }
 });
