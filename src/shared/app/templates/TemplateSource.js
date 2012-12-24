b6db47a2cbb31870a3b8423629b552897679ad5e
Eric Feminella <efeminella@gmail.com>
2012-12-24 12:11:56 -0500

    Update src/shared/app/templates/TemplateSource.js

--- src/shared/app/templates/TemplateSource.js
+++ src/shared/app/templates/TemplateSource.js
@@ -3,6 +3,8 @@ define( function( require ) {
     return {
     	// loads the some-view.tpl based on the application's context: 
     	// either mobile or desktop
+        // mobile/app/templates/some-view.tpl
+        // desktop/app/templates/some-view.tpl
 	someViewTpl: require('text!app/templates/some-view.tpl')
     }
 });
